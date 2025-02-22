323f1133d205d4e2e36ff601dacbe4fd0b8c11ee
kboddya <kboddya@gmail.com>
2025-02-22 21:35:05 +0200

    Addet bundels

--- BookShopProject/App_Start/BundleConfig.cs
+++ BookShopProject/App_Start/BundleConfig.cs
@@ -1,11 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Web;
+using System.Web.Optimization;
 
-namespace BookShopProject.App_Start
+namespace BookShopProject
 {
-	public class Class1
-	{
-	}
+    public class BundleConfig
+    {
+        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle("~/bundles/normalize/css").Include("~/Content/Styles/normalize.css",
+                new CssRewriteUrlTransform()));
+            bundles.Add(new StyleBundle("~/bundles/style/css").Include("~/Content/Styles/style.css",
+                new CssRewriteUrlTransform()));
+            bundles.Add(new StyleBundle("~/bundles/vendor/css").Include("~/Content/Styles/vendor.css",
+                new CssRewriteUrlTransform()));
+            
+            bundles.Add(new ScriptBundle("~/bundles/jquery/js").Include("~/Scrips/jquery-1.11.0.min.js"));
+            bundles.Add(new ScriptBundle("~/bundles/modernizr/js").Include("~/Scrips/modernizr.js"));
+            bundles.Add(new ScriptBundle("~/bundles/plugins/js").Include("~/Scrips/plugins.js"));
+            bundles.Add(new ScriptBundle("~/bundles/script/js").Include("~/Scrips/script.js"));
+            bundles.Add(new ScriptBundle("~/bundles/slideNav/js").Include("~/Scrips/slideNav.js"));
+            bundles.Add(new ScriptBundle("~/bundles/slideNav/min/js").Include("~/Scrips/slideNav.min.js"));
+        }
+    }
 }
--- BookShopProject/Global.asax.cs
+++ BookShopProject/Global.asax.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
 using System.Web.Security;
 using System.Web.SessionState;
@@ -14,8 +15,9 @@ namespace BookShopProject
         void Application_Start(object sender, EventArgs e)
         {
             // Code that runs on application startup
-           AreaRegistration.RegisterAllAreas();
-           RouteConfig.RegisterRoutes(RouteTable.Routes);
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
