b4840232d65306a78281b8af033d8ca43329e0cc
kboddya <kboddya@gmail.com>
2025-02-27 14:30:13 +0200

    Added Genres page and fixed redirect when touch on logo

--- BookShopProject/App_Start/BundleConfig.cs
+++ BookShopProject/App_Start/BundleConfig.cs
@@ -8,11 +8,14 @@ namespace BookShopProject
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new StyleBundle("~/bundles/styles").Include("~/Content/Styles/bootstrap.min.css", "~/Content/Styles/normalize.css", "~/Content/Styles/style.css", "~/Content/Styles/vendor.css"));
+            bundles.Add(new StyleBundle("~/bundles/styles").Include("~/Content/Styles/bootstrap.min.css",
+                "~/Content/Styles/normalize.css", "~/Content/Styles/style.css", "~/Content/Styles/vendor.css",
+                "~/Content/Styles/custom.css"));
 
-            
+
             bundles.Add(new ScriptBundle("~/bundles/scrips").Include("~/Scrips/bootstrap.bundle.min.js",
-                "~/Scrips/jquery-1.11.0.min.js", "~/Scrips/modernizr.js", "~/Scrips/plugins.js", "~/Scrips/script.js","~/Scrips/slideNav.js","~/Scrips/slideNav.min.js"));
+                "~/Scrips/jquery-1.11.0.min.js", "~/Scrips/modernizr.js", "~/Scrips/plugins.js", "~/Scrips/script.js",
+                "~/Scrips/slideNav.js", "~/Scrips/slideNav.min.js"));
         }
     }
 }
---
+++ BookShopProject/Controllers/GenreController.cs
@@ -0,0 +1,13 @@
+using System.Web.Mvc;
+
+namespace BookShopProject.Controllers
+{
+    public class GenreController : Controller
+    {
+        // GET
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
