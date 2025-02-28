3f5054a9e90bf7752c7493057a3c650328d879c3
Alexandra <alexandra.nicipurenco@isa.utm.md>
2025-02-28 14:30:49 +0200

    Updated references

---
+++ BookShopProject/Controllers/AboutController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BookShopProject.Controllers
+{
+    public class AboutController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SendMessage(string yourName, string yourEmail, string subject, string message)
+        {
+            TempData["SuccessMessage"] = "Your message has been sent successfully!";
+
+            return RedirectToAction("Index");
+        }
+    }
+}
