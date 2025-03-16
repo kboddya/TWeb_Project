bd1ea001e6e517755e27e1e58c0ba7c390d9866b
Alexandra <alexandra.nicipurenco@isa.utm.md>
2025-03-16 20:47:45 +0200

    added pages with book reviews, offers and some changes on the about us page

--- BookShopProject/App_Start/BundleConfig.cs
+++ BookShopProject/App_Start/BundleConfig.cs
@@ -11,12 +11,13 @@ namespace BookShopProject
             bundles.Add(new StyleBundle("~/bundles/styles").Include("~/Content/Styles/custom.css",
                 "~/Content/Styles/bootstrap.min.css",
                 "~/Content/Styles/normalize.css", "~/Content/Styles/style.css", "~/Content/Styles/vendor.css",
-                "~/Content/Styles/icomoon.css"));
+                "~/Content/Styles/icomoon.css", "~/Content/slick.css", "~/Content/slick-theme.css", "~/Content/about.css", "~/Content/offer.css", "~/Content/review.css"));
 
 
             bundles.Add(new ScriptBundle("~/bundles/scrips").Include("~/Scrips/bootstrap.bundle.min.js",
                 "~/Scrips/jquery-1.11.0.min.js", "~/Scrips/modernizr.js", "~/Scrips/plugins.js", "~/Scrips/script.js",
-                "~/Scrips/slideNav.js", "~/Scrips/slideNav.min.js"));
+                "~/Scrips/slideNav.js", "~/Scrips/slideNav.min.js", "~/Scrips/about.js", "~/Scrips/slick.min.js"));
+
         }
     }
 }
---
+++ BookShopProject/Scrips/about.js
@@ -0,0 +1,15 @@
+﻿
+$(function () {
+    $('.main-slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        arrows: true,
+        dots: true,
+        adaptiveHeight: true,
+        prevArrow: $('.prev'),
+        nextArrow: $('.next'),
+    });
+});
+
