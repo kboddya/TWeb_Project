970bad0c6e8a3db06a681d101b7c28a031a480ee
Alina <alina.vladok@gmail.com>
2025-03-18 11:39:32 +0200

    Corrected SessionBL file

--- eUseControl.BusinessLogic/SessionBL.cs
+++ eUseControl.BusinessLogic/SessionBL.cs
@@ -1,14 +1,30 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using BookShopProject.Domain.Entities.User;
 using eUseControl.BusinessLogic.Core;
 using eUseControl.BusinessLogic.Interfaces;
 
 namespace eUseControl.BusinessLogic
 {
-    class SessionBL: UserApi, ISession
+    class SessionBL : UserApi, ISession
     {
+        public UserLoginResult UserLogin(ULoginData data)
+        {
+            if (data.Credential == "admin" && data.Password == "password")
+            {
+                return new UserLoginResult
+                {
+                    Status = true,
+                    StatusMsg = "Login successful"
+                };
+            }
+            else
+            {
+                return new UserLoginResult
+                {
+                    Status = false,
+                    StatusMsg = "Invalid credentials"
+                };
+            }
+        }
     }
 }
