df90b642529082087831071e98892e989691f568
Alina <alina.vladok@gmail.com>
2025-03-18 09:34:48 +0200

    Bussiness logic try

---
+++ BookShopProject/Repositories/ICartRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BookShopProject.Models;
+
+namespace BookShopProject.Repositories
+{
+    public interface ICartRepository
+    {
+        IEnumerable<CartItem> GetAllCartItems();
+        CartItem GetCartItemById(int id);
+        void AddCartItem(CartItem cartItem);
+        void UpdateCartItem(CartItem cartItem);
+        void DeleteCartItem(int id);
+    }
+}
+
