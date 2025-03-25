a5454b4b1d5666966b5486961fe9d226d80bb871
Bohdan Kondratiuk <kboddya@gmail.com>
2025-03-25 19:22:15 +0200

    Add new model files

---
+++ BookShopProject/Models/Article.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BookShopProject.Models
+{
+    public class Article
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Image { get; set; }
+        public string Content { get; set; }
+        public string PublishedDate { get; set; }
+    }
+
+    public class ArticleList
+    {
+        public List<Article> Articles { get; set; }
+    }
+}
---
+++ BookShopProject/Models/BookList.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace BookShopProject.Models
+{
+    public class BookList
+    {
+        public string NameOfList { get; set; }
+        public List<Book> Products { get; set; }
+    }
+}
